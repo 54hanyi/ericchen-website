65119daa9b5e208e1e67b42d87ef3248b7c780c4
陳翰毅 <tp6c04u4456@yahoo.com.tw>
2025-03-20 22:10:47 +0800

    修正gh-page圖片無法顯示路徑問題測試

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  // base: "/ericchen-website/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // npm run dev → mode 為 development
+    // npm run build → mode 為 production
+    base: mode === "production" ? "/ericchen-website/" : "/",
+  };
 });
